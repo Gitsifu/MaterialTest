6454542d86924ff6949199ec1837bec325ed0caf
sifu <2587107273.@qq.com>
2017-06-15 11:22:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/materialtest/Fruit.java
@@ -0,0 +1,25 @@
+package com.example.materialtest;
+
+/**
+ * Created by sifu on 2017/6/14.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
